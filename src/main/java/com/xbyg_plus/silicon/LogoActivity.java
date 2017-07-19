9d94a6a408b754d85cc7b2996abdbc6d98f4fd55
xbyg <minecraft.xbyg@gmail.com>
2017-07-19 20:17:30 +0800

    Add LogoActivity and follow semantic versioning

---
+++ src/main/java/com/xbyg_plus/silicon/LogoActivity.java
@@ -0,0 +1,24 @@
+package com.xbyg_plus.silicon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class LogoActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, LoginActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 2500);
+    }
+}
